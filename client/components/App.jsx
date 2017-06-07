bf5314733f255c073cda1eb52c9b83f1934b5d26
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-06-08 00:02:45 +1200

    create project structure

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateReactClass from 'create-react-class'
+
+export default CreateReactClass({
+  render() {
+    return (
+      <div>
+      <h1>Where should I go?</h1>
+      </div>
+    )
+  }
+})
